6dda6f4b6396c9927809abc51bd270b107ffe126
neojou <neojou@gmail.com>
2022-12-05 21:45:59 +0800

    JavaFX-demo : set Scene/VBox/Text manually without using fxml
    Signed-off-by: neojou <neojou@gmail.com>

--- javafx/JavaFX-demo/src/main/java/com/nj/javafxdemo/HelloApplication.java
+++ javafx/JavaFX-demo/src/main/java/com/nj/javafxdemo/HelloApplication.java
@@ -3,6 +3,8 @@ package com.nj.javafxdemo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Text msg = new Text("Hello JavaFx");
+        VBox root = new VBox();
+        root.getChildren().add(msg);
+        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(root, 300, 50);
         stage.setTitle("Hello JavaFX Application");
         stage.setScene(scene);
         stage.show();
