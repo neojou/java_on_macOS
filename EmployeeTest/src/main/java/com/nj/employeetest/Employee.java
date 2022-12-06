e689b7fee22e0f23c7749659bf6183ec7cb8eba9
neojou <neojou@gmail.com>
2022-12-06 20:06:42 +0800

    EmplyeeTest - use JavaFX properties and bindings
    Signed-off-by: neojou <neojou@gmail.com>

--- EmployeeTest/src/main/java/com/nj/employeetest/Employee.java
+++ EmployeeTest/src/main/java/com/nj/employeetest/Employee.java
@@ -1,23 +1,40 @@
 package com.nj.employeetest;
 
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
-//import javafx.beans.
+import javafx.beans.binding.NumberBinding;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ObservableValue;
 
 public class Employee {
-    private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 
     private String name;
-    private double salary;
+    DoubleProperty salary;
+    final static double TAX_PERCENT = 20.0 / 100;
+    NumberBinding tax;
 
     public Employee() {
         this.name = "John Doe";
-        this.salary = 1000.0;
+        this.salary = new SimpleDoubleProperty(1000.0);
+        this.salary.addListener(Employee::changed);
+        tax = this.salary.multiply(TAX_PERCENT);
     }
 
     public Employee(String name, double salary) {
         this.name = name;
-        this.salary = salary;
+        this.salary = new SimpleDoubleProperty(salary);
+        this.salary.addListener(Employee::changed);
+        tax = this.salary.multiply(TAX_PERCENT);
+    }
+
+    public static void changed(ObservableValue<? extends Number> prop,
+                               Number oldValue, Number newValue) {
+            System.out.print("Salary has changed. ");
+            System.out.print("Old:" + oldValue);
+            System.out.println(", New:" + newValue);
+    }
+
+    public void show() {
+        System.out.println("Salary:" + salary.getValue() + ", Tax: " + tax.getValue());
     }
 
     public String getName() {
@@ -29,21 +46,11 @@ public class Employee {
     }
 
     public double getSalary() {
-        return salary;
+        return salary.getValue();
     }
 
     public void setSalary(double newSalary) {
-        double oldSalary = this.salary;
-        this.salary = newSalary;
-        pcs.firePropertyChange("salary", oldSalary, newSalary);
-    }
-
-    public void addPropertyChangeListener(PropertyChangeListener listener) {
-        pcs.addPropertyChangeListener(listener);
-    }
-
-    public void removePropertyChangeListener(PropertyChangeListener listener) {
-        pcs.removePropertyChangeListener(listener);
+        salary.set(newSalary);
     }
 
 }
--- EmployeeTest/src/main/java/com/nj/employeetest/HelloApplication.java
+++ EmployeeTest/src/main/java/com/nj/employeetest/HelloApplication.java
@@ -18,34 +18,18 @@ public class HelloApplication extends Application {
         stage.show();
 
         final Employee el = new Employee("Bob", 2000.0);
-
-        computeTax(el.getSalary());
-        el.addPropertyChangeListener(
-                HelloApplication::handlePropertyChange);
+        el.show();
 
         el.setSalary(3000.0);
+        el.show();
         el.setSalary(3000.0);
+        el.show();
         el.setSalary(6000.0);
-
+        el.show();
     }
 
     public static void main(String[] args) {
         launch();
-    }
-
-    public static void handlePropertyChange(PropertyChangeEvent e) {
-        if ("salary".equals(e.getPropertyName())) {
-            System.out.print("Salary has changed. ");
-            System.out.print("Old:" + e.getOldValue());
-            System.out.println(", New:" + e.getNewValue());
-            computeTax((Double) e.getNewValue());
-        }
-    }
-
-    public static void computeTax(double salary) {
-        final double TAX_PERCENT = 20.0 / 100;
-        double tax = salary * TAX_PERCENT;
-        System.out.println("Salary:" + salary + ", Tax: " + tax);
     }
 
 }
