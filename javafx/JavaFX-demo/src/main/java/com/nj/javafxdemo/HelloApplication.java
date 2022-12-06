d31dd17f2b55c4a484579f0b991ab539291ae3b3
neojou <neojou@gmail.com>
2022-12-06 17:38:32 +0800

    JavaFX-demo: add exit button
    Signed-off-by: neojou <neojou@gmail.com>

--- javafx/JavaFX-demo/src/main/java/com/nj/javafxdemo/HelloApplication.java
+++ javafx/JavaFX-demo/src/main/java/com/nj/javafxdemo/HelloApplication.java
@@ -1,8 +1,12 @@
 package com.nj.javafxdemo;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -14,8 +18,11 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Text msg = new Text("Hello JavaFx");
+        // Create a button with "Exit"
+        Button exitBtn = new Button("Exit");
+        exitBtn.setOnAction(e -> Platform.exit());
         VBox root = new VBox();
-        root.getChildren().add(msg);
+        root.getChildren().addAll(msg, exitBtn);
         //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         Scene scene = new Scene(root, 300, 50);
         stage.setTitle("Hello JavaFX Application");
