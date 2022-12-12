a6e80fa5c5a6db0e85b7a6bda773130272adcfb4
neojou <neojou@gmail.com>
2022-12-12 20:54:45 +0800

    Use Supplier<Stream<String>>
    A stream can only be used once, once a stream has been consumed,
    it can no longer be used, and attempting to do so will cause an
    'IllegalStateException` to be thrown.
    
    Signed-off-by: neojou <neojou@gmail.com>

--- SupplierExample/src/Main.java
+++ SupplierExample/src/Main.java
@@ -1,8 +1,40 @@
+
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 
 public class Main {
+
+    public static void test(Stream<String> s) {
+        List<String> slist = s.collect(Collectors.toList());
+        System.out.println(slist.size());
+
+        Stream<String> s1 = slist.stream();
+        Supplier<Stream<String>> streamSupplier = () -> s1;
+
+        final AtomicInteger counter = new AtomicInteger();
+        streamSupplier.get().parallel()
+                .collect(Collectors.groupingBy(it -> counter.getAndIncrement() / 2))
+                .values()
+                .stream()
+                .forEach(input -> {
+                    System.out.println("input " + input);
+                });
+    }
+
     public static void main(String[] args) {
+        IntStream nums = IntStream.rangeClosed(1, 10);
+        Stream<String> ss = nums.mapToObj(i -> Integer.toString(i));
+        test(ss);
+    }
+
+
+    private void example() {
         // Create a new random number generator
         Random random = new Random();
 
